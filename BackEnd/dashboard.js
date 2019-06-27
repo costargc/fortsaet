2f7ad64f6bb60944dd7c1a6b14f24c78737bc97d
Rodrigo Costa <costa.rgc@gmail.com>
2019-06-26 22:02:22 -0400

    add firebase count to frontpage

--- BackEnd/dashboard.js
+++ BackEnd/dashboard.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyBOUGxfInRpMOwYNv7zTOeht_H7mDSEdF4",
+        apiKey: grabmykey(),
         authDomain: "forsaet-0.firebaseapp.com",
         databaseURL: "https://forsaet-0.firebaseio.com",
         projectId: "forsaet-0",
@@ -34,16 +34,52 @@ $(document).ready(function () {
 
     var analysisResults = {
         categories: natLanguageResults.results.categories,
-        concepts: natLanguageResults.results.concepts.slice(0,10),
+        concepts: natLanguageResults.results.concepts.slice(0, 10),
         emotions: natLanguageResults.results.emotion.document.emotion,
         entities: natLanguageResults.results.entities,
-        keywords: natLanguageResults.results.keywords.slice(0,10),
+        keywords: natLanguageResults.results.keywords.slice(0, 10),
         needs: [lowestNeeds, largestNeeds],
         personality: personalityResults.personality,
         values: personalityResults.values,
     };
-    
+
     database.ref().push(analysisResults);
 
+    function grabmykey() {
+        p1 = "AIzaSyBOUGxfIn";
+        p2 = "RpMOwYNv7zTOeht_H7mDSEdF4";
+
+        p1 = encrypt(p1, -10);
+        p2 = encrypt(p2, -10);
+
+        return p1 + "" + p2
+
+    };
+
+    function encrypt(msg, key) {
+        var encMsg = "";
+
+        for (var i = 0; i < msg.length; i++) {
+            var code = msg.charCodeAt(i);
+
+            // Encrypt only letters in 'A' ... 'Z' interval
+            if (code >= 65 && code <= 65 + 26 - 1) {
+                code -= 65;
+                code = mod(code + key, 26);
+                code += 65;
+            }
+
+            encMsg += String.fromCharCode(code);
+        }
+
+        return encMsg;
+    }
+
+    function mod(n, p) {
+        if (n < 0)
+            n = p - Math.abs(n) % p;
+
+        return n % p;
+    }
 
 });
