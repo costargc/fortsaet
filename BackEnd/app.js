00f1b0a6f4a995eb784aaceb6190f36c3e19e757
Matt Gilliland <mgilliland23@gmail.com>
2019-07-13 20:12:04 -0400

    merged changes from master

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,169 +1,169 @@
-$(document).ready(function () {
-    
-    var firebaseConfig = {
-        apiKey: grabmykey(),
-        authDomain: "forsaet-0.firebaseapp.com",
-        databaseURL: "https://forsaet-0.firebaseio.com",
-        projectId: "forsaet-0",
-        storageBucket: "",
-        messagingSenderId: "260809973088",
-        appId: "1:260809973088:web:fcf11adadec4d424"
-    };
+// $(document).ready(function () {
 
-    firebase.initializeApp(firebaseConfig);
-    var database = firebase.database();
+var firebaseConfig = {
+    apiKey: grabmykey(),
+    authDomain: "forsaet-0.firebaseapp.com",
+    databaseURL: "https://forsaet-0.firebaseio.com",
+    projectId: "forsaet-0",
+    storageBucket: "",
+    messagingSenderId: "260809973088",
+    appId: "1:260809973088:web:fcf11adadec4d424"
+};
 
-    database.ref().on("value", function (snapshot) {
-        console.log(snapshot.numChildren());
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-    
-    function wordCheck(str) {
-        var wordCount = str.split(" ").length;
-        console.log(wordCount);
-        if (wordCount < 100) {
-            $("#display-error").text("Please enter at least 100 words.");
-        }
+database.ref().on("value", function (snapshot) {
+    console.log(snapshot.numChildren());
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+
+function wordCheck(str) {
+    var wordCount = str.split(" ").length;
+    console.log(wordCount);
+    if (wordCount < 100) {
+        $("#display-error").text("Please enter at least 100 words.");
     }
+}
+//$("#analyze").on("click",
+function analyzeResume(content) {
+    event.preventDefault();
 
-    $("#analyze").on("click", function () {
-        event.preventDefault();
-        
-        var natural_language;
-        var personality;
+    var natural_language;
+    var personality;
 
-        $('#loadingmodal').css('display', 'flex');
-        $('#textmodal').css('display', 'none');
+    $('#loadingmodal').css('display', 'flex');
+    $('#textmodal').css('display', 'none');
 
-        // var $captcha = $('#recaptcha'),
-        //     response = grecaptcha.getResponse();
+    // var $captcha = $('#recaptcha'),
+    //     response = grecaptcha.getResponse();
 
-        // if (response.length === 0) {
-        //     $('.msg-error').text("reCAPTCHA is mandatory");
-        //     if (!$captcha.hasClass("error")) {
-        //         $captcha.addClass("error");
-        //     }
-        // } else {
-        //     $('.msg-error').text('');
-        //     $captcha.removeClass("error");
-        //     alert('reCAPTCHA marked');
-        // }
+    // if (response.length === 0) {
+    //     $('.msg-error').text("reCAPTCHA is mandatory");
+    //     if (!$captcha.hasClass("error")) {
+    //         $captcha.addClass("error");
+    //     }
+    // } else {
+    //     $('.msg-error').text('');
+    //     $captcha.removeClass("error");
+    //     alert('reCAPTCHA marked');
+    // }
 
 
 
 
-        value = $("#cv-text-1").val();
+    value = content;
 
-        wordCheck(value);
+    wordCheck(value);
 
-        valueA = JSON.stringify({ features: { concepts: {}, entities: {}, keywords: {}, categories: {}, emotion: {}, sentiment: {}, semantic_roles: {}, syntax: { tokens: { lemma: true, part_of_speech: true }, sentences: true } }, text: value });
-        // console.log(valueA);
+    valueA = JSON.stringify({ features: { concepts: {}, entities: {}, keywords: {}, categories: {}, emotion: {}, sentiment: {}, semantic_roles: {}, syntax: { tokens: { lemma: true, part_of_speech: true }, sentences: true } }, text: value });
+    // console.log(valueA);
 
-        //Call to Natural Language Understanding
-        sessionStorage.clear();
+    //Call to Natural Language Understanding
+    sessionStorage.clear();
 
-        // had to add one ajax call inside the other in order to be able to change to the dashboard page in an syncronous way.
+    // had to add one ajax call inside the other in order to be able to change to the dashboard page in an syncronous way.
 
+    $.ajax({
+        type: 'POST',
+        url: "https://cors-anywhere.herokuapp.com/https://natural-language-understanding-demo.ng.bluemix.net/api/analyze",
+        contentType: 'application/json',
+        data: valueA,
+
+        crossOrigin: true,
+        beforeSend: function (xhr) {
+            Origin: 'https://natural-language-understanding-demo.ng.bluemix.net'
+        },
+        success: function (body) {
+            // console.log(body);
+            natural_language = body;
+            sessionStorage.setItem('natural_language', JSON.stringify(natural_language));
+            callPersonality();
+        },
+        error: function (xhr) {
+            console.log(xhr);
+            $('#loadingmodal').css('display', 'none');
+            $('#textmodal').css('display', 'flex');
+        },
+    })
+
+    //Call to Personality Insights
+    function callPersonality() {
         $.ajax({
             type: 'POST',
-            url: "https://cors-anywhere.herokuapp.com/https://natural-language-understanding-demo.ng.bluemix.net/api/analyze",
-            contentType: 'application/json',
-            data: valueA,
+            url: 'https://cors-anywhere.herokuapp.com/https://personality-insights-demo.ng.bluemix.net/api/profile/text',
 
+            dataType: 'json',
+            data: {
+                text: value
+            },
             crossOrigin: true,
             beforeSend: function (xhr) {
-                Origin: 'https://natural-language-understanding-demo.ng.bluemix.net'
             },
             success: function (body) {
                 // console.log(body);
-                natural_language = body;
-                sessionStorage.setItem('natural_language', JSON.stringify(natural_language));
-                callPersonality();
+                personality = body;
+                sessionStorage.setItem('personality', JSON.stringify(personality));
+
+                console.log(JSON.parse(sessionStorage.getItem('natural_language')));
+                console.log(JSON.parse(sessionStorage.getItem('personality')));
+                // change to dashboard page
+                window.open('./dashboard/index.html', '_self');
+
             },
-            error: function (xhr) {
+            error: function (xhr) { // if error occured
                 console.log(xhr);
                 $('#loadingmodal').css('display', 'none');
                 $('#textmodal').css('display', 'flex');
             },
-        })
+            // dataType: 'html'
+        });
 
-        //Call to Personality Insights
-        function callPersonality() {
-            $.ajax({
-                type: 'POST',
-                url: 'https://cors-anywhere.herokuapp.com/https://personality-insights-demo.ng.bluemix.net/api/profile/text',
+    }
 
-                dataType: 'json',
-                data: {
-                    text: value
-                },
-                crossOrigin: true,
-                beforeSend: function (xhr) {
-                },
-                success: function (body) {
-                    // console.log(body);
-                    personality = body;
-                    sessionStorage.setItem('personality', JSON.stringify(personality));
 
-                    console.log(JSON.parse(sessionStorage.getItem('natural_language')));
-                    console.log(JSON.parse(sessionStorage.getItem('personality')));
-                    // change to dashboard page
-                    window.open('./dashboard/index.html', '_self');
+}
 
-                },
-                error: function (xhr) { // if error occured
-                    console.log(xhr);
-                    $('#loadingmodal').css('display', 'none');
-                    $('#textmodal').css('display', 'flex');
-                },
-                // dataType: 'html'
-            });
 
+function grabmykey() {
+    p1 = "AIzaSyBOUGxfIn";
+    p2 = "RpMOwYNv7zTOeht_H7mDSEdF4";
+
+    p1 = encrypt(p1, -10);
+    p2 = encrypt(p2, -10);
+
+    return p1 + "" + p2
+
+};
+
+function encrypt(msg, key) {
+    var encMsg = "";
+
+    for (var i = 0; i < msg.length; i++) {
+        var code = msg.charCodeAt(i);
+
+        // Encrypt only letters in 'A' ... 'Z' interval
+        if (code >= 65 && code <= 65 + 26 - 1) {
+            code -= 65;
+            code = mod(code + key, 26);
+            code += 65;
         }
 
-
-    })
-
-
-    function grabmykey() {
-        p1 = "AIzaSyBOUGxfIn";
-        p2 = "RpMOwYNv7zTOeht_H7mDSEdF4";
-    
-        p1 = encrypt(p1, -10);
-        p2 = encrypt(p2, -10);
-    
-        return p1 + "" + p2
-    
-    };
-    
-    function encrypt(msg, key) {
-        var encMsg = "";
-    
-        for (var i = 0; i < msg.length; i++) {
-            var code = msg.charCodeAt(i);
-    
-            // Encrypt only letters in 'A' ... 'Z' interval
-            if (code >= 65 && code <= 65 + 26 - 1) {
-                code -= 65;
-                code = mod(code + key, 26);
-                code += 65;
-            }
-    
-            encMsg += String.fromCharCode(code);
-        }
-    
-        return encMsg;
+        encMsg += String.fromCharCode(code);
     }
-    
-    function mod(n, p) {
-        if (n < 0)
-            n = p - Math.abs(n) % p;
-    
-        return n % p;
-    }
-    
+
+    return encMsg;
+}
+
+function mod(n, p) {
+    if (n < 0)
+        n = p - Math.abs(n) % p;
+
+    return n % p;
+}
 
 
-});
+
+    // });
---
+++ BackEnd/pdf-parser.js
@@ -0,0 +1,107 @@
+//Handles the drag & drop logic
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file
+        if (ev.dataTransfer.items[0].kind === 'file') {
+            //Get a copy of the file that was dropped
+            var resumeFile = ev.dataTransfer.items[0].getAsFile();
+            console.log('Filename ' + resumeFile.name);
+            //prepare the PDF file for parsing
+            processFile(resumeFile);
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+
+        }
+    }
+}
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+    $('#drop-zone').css("background-color", "gray");
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
+
+function dragLeaveHandler(ev) {
+    $('#drop-zone').css("background-color", "white");
+}
+
+//Converts the file to the proper format before being passed to the getTextFromPDF function
+function processFile(file) {
+    // Open the file as base64 using FileReader API
+    var fileReader = new FileReader();
+    // Onload of file read the file contents
+    fileReader.onload = function (fileLoadedEvent) {
+        DataURI = fileLoadedEvent.target.result;
+        //Must be converted to binary to work with PDF.js
+        var binary = convertDataURIToBinary(DataURI);
+        //Now pass the converted binary to the getTextFromPDF function
+        getTextFromPDF(binary);
+    };
+    //Convert from PDF to base64
+    fileReader.readAsDataURL(file);
+}
+
+//Thank you Stackoverflow, this is the format that pdf.js expects the file to be in
+var BASE64_MARKER = ';base64,';
+function convertDataURIToBinary(dataURI) {
+    var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    var base64 = dataURI.substring(base64Index);
+    var raw = window.atob(base64);
+    var rawLength = raw.length;
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+    for (var i = 0; i < rawLength; i++) {
+        array[i] = raw.charCodeAt(i);
+    }
+    return array;
+}
+
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.2.2/pdf.worker.js';
+
+// Gets the text from a PDF file using the pdf.js library
+function getTextFromPDF(pdfData) {
+
+    var loadingTask = pdfjsLib.getDocument({ data: pdfData });
+
+    loadingTask.promise.then(function (pdf) {
+        console.log('PDF loaded');
+
+        var numPages = pdf.numPages;
+        var countPromises = []; // collecting all page promises
+
+        for (var i = 1; i <= numPages; i++) {
+            var page = pdf.getPage(i);
+            countPromises.push(page.then(function (page) { // add page promise
+                var textContent = page.getTextContent();
+                return textContent.then(function (text) { // return content promise
+                    return text.items.map(function (s) { return s.str; }).join(''); // value page text 
+                });
+            }));
+        }
+        // Wait for all pages and join text
+        return Promise.all(countPromises).then(function (texts) {
+            console.log(texts.join(''));
+            var resumeText = texts.join('');
+            analyzeResume(resumeText);
+            return resumeText;
+        });
+
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
+
+}
